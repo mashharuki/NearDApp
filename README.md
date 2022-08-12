60cf5c16b4302e1df129d5491c2267f4d36a6c9c
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-08-12 22:11:24 +0900

    レッスン2まで完了！

--- README.md
+++ README.md
@@ -57,13 +57,43 @@ Creating a new NEAR dApp
          npm start
 ```
 
-### テスト実行方法
+### テスト実行方法1
 
 `npm test`
 
 ```cmd
  Passed ✅ gets default message
-    Passed ✅ changes message
+ Passed ✅ changes message
+```
+
+### テスト実行方法2
+
+`cd contract`  
+`cargo test` 
+
+うまく行けば下記の様に出力される。  
+
+```cmd
+    Blocking waiting for file lock on package cache
+    Blocking waiting for file lock on build directory
+   Compiling hotel_booking v1.0.0 (/Users/harukikondo/git/NearDApp/near-hotel-booking-dapp/contract)
+    Finished test [unoptimized + debuginfo] target(s) in 1m 10s
+     Running unittests src/lib.rs (target/debug/deps/hotel_booking-6485072b7a754368)
+
+running 5 tests
+test tests::no_available_room ... ok
+test tests::no_registered_room ... ok
+test tests::add_then_get_available_rooms ... ok
+test tests::add_then_get_registered_rooms ... ok
+test tests::book_room_then_change_status ... ok
+
+test result: ok. 5 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+   Doc-tests hotel_booking
+
+running 0 tests
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
 ```
 
 ### コントラクトをビルドする方法2
@@ -71,17 +101,27 @@ Creating a new NEAR dApp
 `cd contract`  
 `cargo build --target wasm32-unknown-unknown --release`  
 
-ビルドファイルが生成されているか確認する方法  
+#### ビルドファイルが生成されているか確認する方法  
 `ls ./target/wasm32-unknown-unknown/release | grep hotel_booking.wasm`  
 
-Near Walletへログインするコマンド  
+#### Near Walletへログインするコマンド  
 
 `near login`  
 
-サブアカウントを作成するコマンド  
+#### サブアカウントを作成するコマンド  
 `near create-account contract.dev-1660204085773-49134722844982 --masterAccount dev-1660204085773-49134722844982 --initialBalance 5`   
 
-スマートコントラクトをデプロイするコマンド  
+もしうまくアカウントが作成できていれば`~/.near-credentials/testnet`フォルダ配下にアカウント情報(jsonファイル形式)が出力されているはずです。
+
+#### サブアカウントを作成するコマンド(例)
+`near create-account booking1.dev-1660204085773-49134722844982 --masterAccount dev-1660204085773-49134722844982 --initialBalance 5`  
+`near create-account booking2.dev-1660204085773-49134722844982 --masterAccount dev-1660204085773-49134722844982 --initialBalance 5`
+
+#### サブアカウントを削除するコマンド  
+
+`near delete contract.dev-1660204085773-49134722844982 dev-1660204085773-49134722844982`
+
+#### スマートコントラクトをデプロイするコマンド  
 `near deploy --wasmFile target/wasm32-unknown-unknown/release/hotel_booking.wasm --accountId contract.dev-1660204085773-49134722844982`  
 
 うまく行けば下記のような内容が出力される。
@@ -94,7 +134,7 @@ https://explorer.testnet.near.org/transactions/5hr8mjNtk2gwiDp4qezbV3dksQgYk8WRX
 Done deploying to contract.dev-1660204085773-49134722844982
 ```
 
-スマートコントラクトのメソッドをコマンドで呼び出す方法(viewメソッド)   
+#### スマートコントラクトのメソッドをコマンドで呼び出す方法(viewメソッド)   
 
 `near view contract.dev-1660204085773-49134722844982 get_rooms_registered_by_owner '{"owner_id": "dev-1660204085773-49134722844982"}' --accountId dev-1660204085773-49134722844982`
 
@@ -105,7 +145,7 @@ View call: contract.dev-1660204085773-49134722844982.get_rooms_registered_by_own
 []
 ```
 
-Room情報を追加するためのcallメソッドを呼び出すコマンドは次の通り  
+#### Room情報を追加するためのcallメソッドを呼び出すコマンドは次の通り  
 
 `near call contract.dev-1660204085773-49134722844982 add_room_to_owner '{"name": "Sun Room", "image": "https://ibb.co/kgNm4jP", "beds": 1, "description": "This is Sun room.", "location": "Tokyo", "price": "1000000000000000000000000"}' --accountId dev-1660204085773-49134722844982`  
 
@@ -126,9 +166,75 @@ View call: contract.dev-1660204085773-49134722844982.get_rooms_registered_by_own
 ]
 ```
 
-existsメソッドを呼び出すコマンド  
+#### existsメソッドを呼び出すコマンド  
 
 `near view contract.dev-1660204085773-49134722844982 exists '{"owner_id": "dev-1660204085773-49134722844982", "room_name": "Sun Room"}' --accountId dev-1660204085773-49134722844982`
+
+#### get_available_roomsメソッドを呼び出すコマンド
+
+`near call booking1.dev-1660204085773-49134722844982 get_available_rooms '{"room_id": "dev-1660204085773-49134722844982Sun Room", "check_in_date": "2222-01-01"}' --accountId dev-1660204085773-49134722844982`
+
+レスポンス例
+```json
+[
+  {
+    room_id: 'dev-1660204085773-49134722844982Sun Room',
+    owner_id: 'dev-1660204085773-49134722844982',
+    name: 'Sun Room',
+    image: 'https://ibb.co/kgNm4jP',
+    beds: 1,
+    description: 'This is Sun room.',
+    location: 'Tokyo',
+    price: '1000000000000000000000000'
+  }
+]
+```
+
+#### book_roomメソッドを呼び出すコマンド
+
+`near call booking1.dev-1660204085773-49134722844982 book_room '{"room_id": "dev-1660204085773-49134722844982Sun Room", "check_in_date": "2222-01-01"}' --depositYocto=1000000000000000000000000 --accountId booking2.dev-1660204085773-49134722844982`
+
+#### ownerが予約データを確認するためのコマンド
+
+`near view booking1.dev-1660204085773-49134722844982 get_booking_info_for_owner '{"owner_id":"dev-1660204085773-49134722844982"}'`  
+
+レスポンス結果
+```json
+[
+  {
+    room_id: 'dev-1660204085773-49134722844982Sun Room',
+    name: 'Sun Room',
+    check_in_date: '2222-01-01',
+    guest_id: 'booking2.dev-1660204085773-49134722844982',
+    status: 'Available'
+  }
+]
+```
+
+#### 宿泊者が自らの予約データを確認するコマンド
+
+`near view booking1.dev-1660204085773-49134722844982 get_booking_info_for_guest '{"guest_id": "booking2.dev-1660204085773-49134722844982"}'`  
+
+レスポンス結果
+```json
+[
+  {
+    owner_id: 'dev-1660204085773-49134722844982',
+    name: 'Sun Room',
+    check_in_date: '2222-01-01'
+  }
+]
+```
+
+#### チェックインを行うためのコマンド
+
+`near call booking1.dev-1660204085773-49134722844982 change_status_to_stay '{"room_id": "dev-1660204085773-49134722844982Sun Room", "check_in_date": "2222-01-01"}' --accountId dev-1660204085773-49134722844982`
+
+#### チェックアウトを行うためのコマンド
+
+`near call booking1.dev-1660204085773-49134722844982 change_status_to_available '{"room_id": "dev-1660204085773-49134722844982Sun Room", "check_in_date": "2222-01-01", "guest_id": "booking2.dev-1660204085773-49134722844982"}' --accountId dev-1660204085773-49134722844982`  
+
+このコマンドを実行した後に予約データを取得するメソッドを打ち込んで[]が返ってきていれば成功!!
 
 ### テストネットへのデプロイ方法
 
@@ -153,6 +259,25 @@ Starting deployment. Account id: dev-1660204085773-49134722844982, node: https:/
 Transaction Id GKapRwXm8uUs3Lf1jMNf9cPURdnoEg5G2uiDkE1brzwx
 To see the transaction in the transaction explorer, please open this url in your browser
 https://explorer.testnet.near.org/transactions/GKapRwXm8uUs3Lf1jMNf9cPURdnoEg5G2uiDkE1brzwx
+Done deploying to dev-1660204085773-49134722844982
+```
+
+### レッスン2時点でのデプロイ記録
+
+```cmd
+ near-hotel-booking-dap@1.0.0 deploy
+> npm run build:contract && cd contract && near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/hello_near.wasm
+
+
+> near-hotel-booking-dap@1.0.0 build:contract
+> cd contract && rustup target add wasm32-unknown-unknown && cargo build --all --target wasm32-unknown-unknown --release && cp ./target/wasm32-unknown-unknown/release/hotel_booking.wasm ../out/main.wasm
+
+info: component 'rust-std' for target 'wasm32-unknown-unknown' is up to date
+    Finished release [optimized] target(s) in 1.22s
+Starting deployment. Account id: dev-1660204085773-49134722844982, node: https://rpc.testnet.near.org, helper: https://helper.testnet.near.org, file: ./target/wasm32-unknown-unknown/release/hello_near.wasm
+Transaction Id DvxxTXK28ncnM4FUo4ZFvjvTYanijGTGEmcSoEQVma9B
+To see the transaction in the transaction explorer, please open this url in your browser
+https://explorer.testnet.near.org/transactions/DvxxTXK28ncnM4FUo4ZFvjvTYanijGTGEmcSoEQVma9B
 Done deploying to dev-1660204085773-49134722844982
 ```
 
--- near-hotel-booking-dapp/contract/src/lib.rs
+++ near-hotel-booking-dapp/contract/src/lib.rs
@@ -5,9 +5,10 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, AccountId};
+use near_sdk::{env, near_bindgen, AccountId, Promise};
 
 use std::collections::HashMap;
+
 
 // Rooom ID
 type RoomId = String;
@@ -55,6 +56,42 @@ pub struct Room {
 pub struct Contract {
     rooms_per_owner: LookupMap<AccountId, Vec<RoomId>>,
     rooms_by_id: HashMap<RoomId, Room>,
+    // booking room data
+    bookings_per_guest: HashMap<AccountId, HashMap<CheckInDate, RoomId>>,
+}
+
+// Booked Room Data Struct
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct BookedRoom {
+    room_id: RoomId,
+    name: String,
+    check_in_date: CheckInDate,
+    guest_id: AccountId,
+    status: UsageStatus,
+}
+
+// Guest Booked Room data struct
+#[derive(Serialize, Deserialize, Debug, BorshSerialize, BorshDeserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct GuestBookedRoom {
+    owner_id: AccountId,
+    name: String,
+    check_in_date: CheckInDate,
+}
+
+// Avaliable Room Data Struct
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, PartialEq, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct AvailableRoom {
+    room_id: RoomId,
+    owner_id: AccountId,
+    name: String,
+    image: String,
+    beds: u8,
+    description: String,
+    location: String,
+    price: U128,
 }
 
 // Default trait
@@ -64,6 +101,7 @@ impl Default for Contract {
         Self {
             rooms_per_owner: LookupMap::new(b"m"),
             rooms_by_id: HashMap::new(),
+            bookings_per_guest: HashMap::new(),
         }
     }
 }
@@ -132,6 +170,94 @@ impl Contract {
     }
 
     /**
+     * book room function
+     */
+    #[payable]
+    pub fn book_room(&mut self, room_id: RoomId, check_in_date: CheckInDate) {
+        // get room data
+        let room = self
+            .rooms_by_id
+            .get_mut(&room_id)
+            .expect("ERR_NOT_FOUND_ROOM");
+        // get accountID
+        let account_id = env::signer_account_id();
+
+        // sende near
+        let deposit = env::attached_deposit();
+        // chast
+        let room_price: u128 = room.price.clone().into();
+        // check
+        assert_eq!(deposit, room_price, "ERR_DEPOSIT_IS_INCORRECT");
+
+        // call insert methos
+        room.booked_info.insert(check_in_date.clone(), account_id.clone());
+
+        let owner_id = room.owner_id.clone();
+        // call add_booking_to_guest function
+        self.add_booking_to_guest(account_id, room_id, check_in_date);
+
+        // pay near to owner
+        Promise::new(owner_id).transfer(deposit);
+    }
+
+    /**
+     * change status of room function
+     * Stay -> Avaliable 
+     */
+    pub fn change_status_to_available(
+        &mut self,
+        room_id: RoomId,
+        check_in_date: CheckInDate,
+        guest_id: AccountId,
+    ) {
+        // get room data
+        let mut room = self
+            .rooms_by_id
+            .get_mut(&room_id)
+            .expect("ERR_NOT_FOUND_ROOM");
+
+        // remove
+        room.booked_info
+            .remove(&check_in_date)
+            .expect("ERR_NOT_FOUND_DATE");
+
+         // change status to Available
+        room.status = UsageStatus::Available;
+
+        // remove booking room data
+        self.remove_booking_from_guest(guest_id, check_in_date);
+    }
+
+    /**
+     * change status of room function
+     * Avaliable  -> Stay
+     */
+    pub fn change_status_to_stay(&mut self, room_id: RoomId, check_in_date: CheckInDate) {
+        // get room data
+        let mut room = self
+            .rooms_by_id
+            .get_mut(&room_id)
+            .expect("ERR_NOT_FOUND_ROOM");
+
+         // change status to Stay
+        room.status = UsageStatus::Stay { check_in_date };
+    }
+
+    /**
+     * check room status function
+     */
+    pub fn is_available(&self, room_id: RoomId) -> bool {
+        // get room data
+        let room = self.rooms_by_id.get(&room_id).expect("ERR_NOT_FOUND_ROOM");
+
+        if room.status != UsageStatus::Available {
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
      * get rooms data function 
      */
     pub fn get_rooms_registered_by_owner(&self, owner_id: AccountId) -> Vec<RegisteredRoom> {
@@ -178,5 +304,383 @@ impl Contract {
             // no data
             None => registered_rooms,
         }
+    }
+
+    /**
+     * get booking data function
+     */
+    pub fn get_booking_info_for_owner(&self, owner_id: AccountId) -> Vec<BookedRoom> {
+        // create vector
+        let mut booked_rooms = vec![];
+
+        match self.rooms_per_owner.get(&owner_id) {
+            Some(rooms) => {
+                for room_id in rooms.iter() {
+                    let room = self.rooms_by_id.get(room_id).expect("ERR_NOT_FOUND_ROOM");
+                    
+                    if room.booked_info.len() == 0 {
+                        continue;
+                    }
+                    // create booking data
+                    for (date, guest_id) in room.booked_info.clone() {
+                        
+                        let status: UsageStatus;
+
+                        match &room.status {
+                            UsageStatus::Available => status = UsageStatus::Available,
+                            UsageStatus::Stay { check_in_date } => {
+                                if date == check_in_date.clone() {
+                                    status = UsageStatus::Stay {
+                                        check_in_date: check_in_date.clone(),
+                                    }
+                                } else {
+                                    status = UsageStatus::Available;
+                                }
+                            }
+                        }
+                        // create booked room data
+                        let booked_room = BookedRoom {
+                            room_id: room_id.to_string(),
+                            name: room.name.clone(),
+                            check_in_date: date,
+                            guest_id,
+                            status,
+                        };
+                        // push
+                        booked_rooms.push(booked_room);
+                    }
+                }
+                booked_rooms
+            }
+            // no data
+            None => booked_rooms,
+        }
+    }
+
+    /**
+     * get booking info function
+     */
+    pub fn get_booking_info_for_guest(&self, guest_id: AccountId) -> Vec<GuestBookedRoom> {
+        // create vector
+        let mut guest_info: Vec<GuestBookedRoom> = vec![];
+
+        match self.bookings_per_guest.get(&guest_id) {
+            Some(save_booked_info) => {
+                for (check_in_date, room_id) in save_booked_info {
+                    let room = self.rooms_by_id.get(room_id).expect("ERR_NOT_FOUND_ROOM");
+                    // create guest booked room data
+                    let info = GuestBookedRoom {
+                        owner_id: room.owner_id.clone(),
+                        name: room.name.clone(),
+                        check_in_date: check_in_date.clone(),
+                    };
+                    // push
+                    guest_info.push(info);
+                }
+                guest_info
+            }
+            // no data
+            None => guest_info,
+        }
+    }
+
+    /**
+     * get available rooms function
+     */
+    pub fn get_available_rooms(&self, check_in_date: CheckInDate) -> Vec<AvailableRoom> {
+        // create vector
+        let mut available_rooms = vec![];
+
+        for (room_id, room) in self.rooms_by_id.iter() {
+            match room.booked_info.get(&check_in_date) {
+                
+                Some(_) => continue,
+                // if room_id can be booked
+                None => {
+                    // create Avalilable Room data
+                    let available_room = AvailableRoom {
+                        room_id: room_id.clone(),
+                        owner_id: room.owner_id.clone(),
+                        name: room.name.clone(),
+                        beds: room.beds,
+                        image: room.image.clone(),
+                        description: room.description.clone(),
+                        location: room.location.clone(),
+                        price: room.price,
+                    };
+                    // push
+                    available_rooms.push(available_room);
+                }
+            }
+        }
+        available_rooms
+    }
+}
+
+// Private fuctions for Contract
+impl Contract {
+    
+    /**
+     * add booking to guest fuction
+     */
+    fn add_booking_to_guest(
+        &mut self,
+        guest_id: AccountId,
+        room_id: RoomId,
+        check_in_date: CheckInDate,
+    ) {
+        match self.bookings_per_guest.get_mut(&guest_id) {
+            // if guest has already other booking data
+            Some(booked_date) => {
+                booked_date.insert(check_in_date.clone(), room_id);
+            }
+            // first time
+            None => {
+                let mut new_guest_date = HashMap::new();
+                new_guest_date.insert(check_in_date.clone(), room_id);
+                self.bookings_per_guest.insert(guest_id, new_guest_date);
+            }
+        }
+    }
+
+    /**
+     * remove booking data function
+     */
+    fn remove_booking_from_guest(&mut self, guest_id: AccountId, check_in_date: CheckInDate) {
+        // get booking data 
+        let book_info = self
+            .bookings_per_guest
+            .get_mut(&guest_id)
+            .expect("ERR_NOT_FOUND_GUEST");
+
+        // remove
+        book_info
+            .remove(&check_in_date)
+            .expect("ERR_NOT_FOUND_BOOKED");
+
+        // if book infomation is empty, booking data is also removed
+        if book_info.is_empty() {
+            self.bookings_per_guest.remove(&guest_id);
+        }
+    }
+}
+
+/**
+ * ========================================================
+ * test code
+ * ========================================================
+ */
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    /**
+     * create virtual Blockchain env
+     */
+    fn get_context(is_view: bool) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .predecessor_account_id(accounts(0))
+            .signer_account_id(accounts(1))
+            // 使用するメソッドをbooleanで指定(viewメソッドはtrue, changeメソッドはfalse)
+            .is_view(is_view);
+        builder
+    }
+
+    /**
+     * test code for getting registered room data
+     */
+    #[test]
+    fn add_then_get_registered_rooms() {
+        // call get_context fuction
+        let context = get_context(false);
+        testing_env!(context.build());
+
+        let mut contract = Contract::default();
+        // add booking data
+        contract.add_room_to_owner(
+            "101".to_string(),
+            "test.img".to_string(),
+            1,
+            "This is 101 room".to_string(),
+            "Tokyo".to_string(),
+            U128(10),
+        );
+        // add booking data
+        contract.add_room_to_owner(
+            "201".to_string(),
+            "test.img".to_string(),
+            1,
+            "This is 201 room".to_string(),
+            "Tokyo".to_string(),
+            U128(10),
+        );
+
+        // get accountID
+        let owner_id = env::signer_account_id();
+        // get all booking data
+        let all_rooms = contract.get_rooms_registered_by_owner(owner_id);
+        // check
+        assert_eq!(all_rooms.len(), 2);
+    }
+
+    /**
+     * test code for getting no booking data
+     */
+    #[test]
+    fn no_registered_room() {
+        // call get_context fuction
+        let context = get_context(true);
+        testing_env!(context.build());
+        let contract = Contract::default();
+        // get booking data
+        let no_registered_room = contract.get_rooms_registered_by_owner(accounts(0));
+        // check
+        assert_eq!(no_registered_room.len(), 0);
+    }
+
+    /**
+     * test code for adding booking data
+     */
+    #[test]
+    fn add_then_get_available_rooms() {
+        // call get_context fuction
+        let mut context = get_context(false);
+        testing_env!(context.build());
+
+        let mut contract = Contract::default();
+        // add booking data
+        contract.add_room_to_owner(
+            "101".to_string(),
+            "test.img".to_string(),
+            1,
+            "This is 101 room".to_string(),
+            "Tokyo".to_string(),
+            U128(10),
+        );
+        // add booking data
+        contract.add_room_to_owner(
+            "201".to_string(),
+            "test.img".to_string(),
+            1,
+            "This is 201 room".to_string(),
+            "Tokyo".to_string(),
+            U128(10),
+        );
+
+        // `get_available_rooms`をコールするアカウントを設定
+        testing_env!(context.signer_account_id(accounts(2)).build());
+        let available_rooms = contract.get_available_rooms("2222-01-01".to_string());
+        // check
+        assert_eq!(available_rooms.len(), 2);
+    }
+
+    /**
+     * test code for getting no available room data
+     */
+    #[test]
+    fn no_available_room() {
+        // call get_context fuction
+        let context = get_context(true);
+        testing_env!(context.build());
+        let contract = Contract::default();
+        // call get_available_rooms function
+        let available_rooms = contract.get_available_rooms("2222-01-01".to_string());
+        assert_eq!(available_rooms.len(), 0);
+    }
+
+    /**
+     * test code for changing booking room status
+     */
+    #[test]
+    fn book_room_then_change_status() {
+        // call get_context fuction
+        let mut context = get_context(false);
+
+        // init balance
+        context.account_balance(10);
+        context.attached_deposit(10);
+
+        testing_env!(context.build());
+        // get signer info
+        let owner_id = env::signer_account_id();
+        let mut contract = Contract::default();
+
+        // call for add_room_to_owner function
+        contract.add_room_to_owner(
+            "101".to_string(),
+            "test.img".to_string(),
+            1,
+            "This is 101 room".to_string(),
+            "Tokyo".to_string(),
+            U128(10),
+        );
+
+        ///////////////////
+        // CHECK BOOKING //
+        ///////////////////
+        // set accountID
+        testing_env!(context.signer_account_id(accounts(2)).build());
+
+        let check_in_date: String = "2222-01-01".to_string();
+        // call get_available room data
+        let available_rooms = contract.get_available_rooms(check_in_date.clone());
+
+        // call book_room function
+        contract.book_room(available_rooms[0].room_id.clone(), check_in_date.clone());
+
+        // get booking room datas
+        let booked_rooms = contract.get_booking_info_for_owner(owner_id.clone());
+        // check
+        assert_eq!(booked_rooms.len(), 1);
+        assert_eq!(booked_rooms[0].check_in_date, check_in_date);
+        assert_eq!(booked_rooms[0].guest_id, accounts(2));
+
+        
+        let guest_booked_rooms = contract.get_booking_info_for_guest(accounts(2));
+        // check data for guest
+        assert_eq!(guest_booked_rooms.len(), 1);
+        assert_eq!(guest_booked_rooms[0].owner_id, owner_id);
+
+        /////////////////////////
+        // CHECK CHANGE STATUS //
+        /////////////////////////
+        // set accountID
+        testing_env!(context.signer_account_id(accounts(1)).build());
+
+        // check status
+        let is_available = contract.is_available(booked_rooms[0].room_id.clone());
+        // check
+        assert_eq!(is_available, true);
+
+        // change status（Available -> Stay）
+        contract.change_status_to_stay(booked_rooms[0].room_id.clone(), check_in_date.clone());
+        // get booking room infomations
+        let booked_rooms = contract.get_booking_info_for_owner(owner_id.clone());
+        // check
+        assert_ne!(booked_rooms[0].status, UsageStatus::Available);
+
+        // check status
+        let is_available = contract.is_available(booked_rooms[0].room_id.clone());
+        // check
+        assert_eq!(is_available, false);
+
+        // change status（Stay -> Available）
+        contract.change_status_to_available(
+            available_rooms[0].room_id.clone(),
+            check_in_date.clone(),
+            booked_rooms[0].guest_id.clone(),
+        );
+
+        // check
+        let booked_rooms = contract.get_booking_info_for_owner(owner_id.clone());
+        assert_eq!(booked_rooms.len(), 0);
+
+        // check
+        let guest_booked_info = contract.get_booking_info_for_guest(accounts(2));
+        assert_eq!(guest_booked_info.len(), 0);
     }
 }
